237e29b079d893e0c9224c1e9b895a11e616f5bd
Abduvohid123 <abduvohid.mirzamahmudov@gmal.com>
2022-01-19 13:17:57 +0500

    composerda autoload sozlandi va fayllarga ulandi

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'ab97091d451261187b6238682809a7526c72a705',
+    'name' => 'aabduvohid/framework',
+  ),
+  'versions' => 
+  array (
+    'aabduvohid/framework' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'ab97091d451261187b6238682809a7526c72a705',
+    ),
+  ),
+);
