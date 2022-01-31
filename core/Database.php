eae502c9cd1583a6c8fad79105e7559bb8d7b212
Abduvohid123 <abduvohid.mirzamahmudov@gmal.com>
2022-01-31 10:02:02 +0500

    DAtabase da createTAbleMigrations(), bazaga_yozilgan_migrationlar() , runMigrations() yozildi  migrations.php orqali chaqirildi

--- core/Database.php
+++ core/Database.php
@@ -9,11 +9,36 @@ class Database
 
     public function __construct(array $config)
     {
-        $dsn=$config['dsn']??'';
-        $user=$config['user']??'';
-        $password=$config['password'];
-        $this->pdo= new \PDO($dsn,$user,$password);
-        $this->pdo->setAttribute(\PDO::ATTR_ERRMODE,\PDO::ERRMODE_EXCEPTION);
+        $dsn = $config['dsn'] ?? '';
+        $user = $config['user'] ?? '';
+        $password = $config['password'];
+        $this->pdo = new \PDO($dsn, $user, $password);
+        $this->pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
     }
 
+
+    public function runMigrations()
+    {
+
+        $this->createTAbleMigrations();
+        var_dump(        $this->bazaga_yozilgan_migrationlar());
+    }
+
+    public function createTAbleMigrations()
+    {
+        $this->pdo->exec(
+            "create table if not exists migrations (
+                      id int auto_increment primary key ,
+                      migration varchar(256),
+                      created_at timestamp default current_timestamp
+                      ) ENGINE=INNODB;"
+        );
+    }
+
+    public function bazaga_yozilgan_migrationlar()
+    {
+       $natija= $this->pdo->prepare('select migration from migrations');
+       $natija->execute();
+       return $natija->fetchAll(\PDO::FETCH_COLUMN); // sub massiv li datani oddiy massiv qilib qaytaradi
+    }
 }
